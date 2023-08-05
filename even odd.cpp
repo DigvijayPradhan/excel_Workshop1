d634eb7be08e48304e5154a26545cf9ab6a79994
DigvijayPradhan <digvijaypradhan3@gmail.com>
2023-08-05 17:45:58 +0530

    first commit

---
+++ even odd.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int no;
+	printf (" \nENTER YOUR No. TO CHECK WHETHER THE NO. IS ODD OR EVEN \n");
+	scanf("%d",&no);
+	if(no%2!=0)	
+		printf("odd no.");
+	else
+		printf("even no.");
+	return 0;
+}
+
